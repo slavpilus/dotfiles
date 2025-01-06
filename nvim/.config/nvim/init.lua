623ff3d6006a1a5e23ef46a0d194d96d2f460626
Slav Pilus <slav@pilus.me.uk>
2025-01-06 06:20:10 +0000

    adding nvim config

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,9 @@
+vim.cmd("set autoindent")
+vim.cmd("set expandtab")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set smartindent")
+vim.cmd("set softtabstop=2")
+vim.cmd("set tabstop=2")
+
+require("config.lazy")
+
---
+++ nvim/.config/nvim/lua/plugins/basic.lua
@@ -0,0 +1,4 @@
+return {
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
+}
